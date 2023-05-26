0b673c1930ca35fa5def783763d8c8f2344a42da
Zoe Guidry <oyezoey@gmail.com>
2023-05-26 14:20:48 -0500

    lab completed.

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        last_item = items.pop()
+        return ', '.join(items) + ', and ' + last_item
